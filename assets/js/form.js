bbfcdb66385ea25012972a2180e60e851b0eeb6e
DESKTOP-MKH8SQO\quock <quockhanhtruong166@gmail.com>
2023-03-07 15:18:50 +0700

    change select option

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,16 +2,7 @@ const clearValue = (name) => {
   if (name == "legion") {
     document.querySelector("[list='legion']").value = "";
   }
-  if (name == "income-type") {
+  if (name == "incomeType") {
     document.querySelector("#income-type").value = "Vui lòng chọn";
   }
 };
-
-// const focusFn = (name) => {
-//   if (name == "legion") {
-//     document.querySelector("[list='legion']").focus();
-//   }
-//   if (name == "income-type") {
-//     document.querySelector("#income-type").focus();
-//   }
-// };
